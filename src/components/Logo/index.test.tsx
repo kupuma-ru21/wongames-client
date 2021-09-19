25a3d627b2106a7c3ded3579aee28b9d0d11befe
kupuma-ru21 <kupumaru21@gmail.com>
2021-09-19 14:52:46 +0900

    feat: add option to hideOnMobile

--- src/components/Logo/index.test.tsx
+++ src/components/Logo/index.test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
+import 'jest-styled-components';
 import Logo from '.';
 
 describe('<Logo />', () => {
@@ -29,5 +30,14 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem',
     });
+  });
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo size="large" hideOnMobile />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width: 768px)' }
+    );
   });
 });
--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 import type { LogoProps } from '.';
 
 const wrapperModifiers = {
@@ -10,11 +11,28 @@ const wrapperModifiers = {
     width: 20rem;
     height: 5.9rem;
   `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+      .text {
+        display: none;
+      }
+
+    `}
+  `,
 };
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
     ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `;
