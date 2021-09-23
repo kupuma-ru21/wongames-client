fded8ba993b37422b578dc2c46df132e901c9f57
kupuma-ru21 <kupumaru21@gmail.com>
2021-09-23 15:53:56 +0900

    feat: add Button fullwidth option

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -5,13 +5,13 @@ import type {
 } from 'styled-components';
 import type { ButtonProps } from '.';
 
-type WrapperProps = Pick<ButtonProps, 'size'>;
+type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>;
 type WrapperModifiersKey = Required<WrapperProps>['size'];
+type ReturnStyleFunc = (theme: DefaultTheme) => FlattenSimpleInterpolation;
+
 type WrapperModifiers = {
-  [key in WrapperModifiersKey]: (
-    theme: DefaultTheme
-  ) => FlattenSimpleInterpolation;
-};
+  [key in WrapperModifiersKey]: ReturnStyleFunc;
+} & { fullWidth: () => FlattenSimpleInterpolation };
 
 const wrapperModifiers: WrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -28,10 +28,13 @@ const wrapperModifiers: WrapperModifiers = {
     font-size: ${theme.font.sizes.medium};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
   `,
+  fullWidth: () => css`
+    width: 100%;
+  `,
 };
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size }) => css`
+  ${({ theme, size, fullWidth }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
@@ -39,5 +42,6 @@ export const Wrapper = styled.button<WrapperProps>`
     padding: ${theme.spacings.xxsmall};
 
     ${!!size && wrapperModifiers[size](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
   `}
 `;
