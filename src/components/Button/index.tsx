2e430974c9ba42608b7df34a17b368887c1b97ca
kupuma-ru21 <kupumaru21@gmail.com>
2021-10-16 13:19:54 +0900

    feat: add Button to support anchor/button types

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,16 @@
-import type { MouseEvent } from 'react';
+import type { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react';
 import * as S from './styles';
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>;
+
 export type ButtonProps = {
-  children?: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
   fullWidth?: boolean;
   icon?: JSX.Element;
-  onClick?: () => (event: MouseEvent<HTMLButtonElement>) => void;
-};
+  as?: React.ElementType;
+} & ButtonTypes;
 
 const Button = ({
   children,
