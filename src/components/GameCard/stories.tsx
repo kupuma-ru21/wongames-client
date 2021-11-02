c6974265762271e28a6e7654cb593356e109d1cf
kupuma-ru21 <kupumaru21@gmail.com>
2021-11-02 22:28:48 +0900

    feat: add promotionalPrice and test skeleton

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -10,7 +10,9 @@ export default {
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00',
   },
+  parameters: { backgrounds: { default: 'dark' } },
 } as Meta;
 
 export const Default: Story<GameCardProps> = (args) => {
