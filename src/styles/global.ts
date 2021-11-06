395575d18cc1c79132ee77c5d3307ddc84a377ee
kupuma-ru21 <kupumaru21@gmail.com>
2021-11-06 23:06:09 +0900

    feat: add style to global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,6 +31,10 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before, &::after {
+      box-sizing: inherit;
+    }
   }
   ${({ theme }) => css`
     html {
@@ -39,6 +43,7 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      background-color: ${theme.colors.mainBg};
     }
   `}
 
