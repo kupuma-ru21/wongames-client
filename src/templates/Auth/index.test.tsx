f9215e8c9b31ee51954887e0b98963c8d585c6e6
kupuma-ru21 <kupumaru21@gmail.com>
2021-11-28 14:57:37 +0900

    feat: add Auth template tests

--- src/templates/Auth/index.test.tsx
+++ src/templates/Auth/index.test.tsx
@@ -1,4 +1,5 @@
 import { renderWithTheme } from 'utils/tests/helpers';
+import { screen } from '@testing-library/react';
 import Auth from '.';
 
 describe('<Auth />', () => {
@@ -8,5 +9,25 @@ describe('<Auth />', () => {
         <input type="text" />
       </Auth>
     );
+
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
+
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i,
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 });
