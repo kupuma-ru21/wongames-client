7eb50015b754a523b981ff4820a2c6c23aa4f91c
kupuma-ru21 <kupumaru21@gmail.com>
2021-12-26 23:04:59 +0900

    feat: create GameItem initial structure

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import GameItem from '.';
+import type { GameItemProps } from '.';
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00',
+  },
+} as Meta;
+
+export const Default: Story<GameItemProps> = (args) => {
+  return <GameItem {...args} />;
+};
