cc28a4bd516d9077988b610962284f50827aba7e
kupuma-ru21 <kupumaru21@gmail.com>
2021-12-31 13:43:39 +0900

    feat: add CardsList

---
+++ src/components/CardsList/stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import cardsMock from 'components/PaymentOptions/mock';
+import type { CardsListProps } from '.';
+import CardsList from '.';
+
+export default {
+  title: 'Profile/CardsList',
+  component: CardsList,
+  args: { cards: cardsMock },
+} as Meta;
+
+export const Default: Story<CardsListProps> = (args) => {
+  return (
+    <div style={{ maxWidth: 850, margin: 'auto' }}>
+      <CardsList {...args} />
+    </div>
+  );
+};
