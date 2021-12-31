cdd3365906c29c040369e357974e1e8a06ad09c8
kupuma-ru21 <kupumaru21@gmail.com>
2022-01-01 01:00:21 +0900

    feat: add /profile/cards page

--- src/pages/profile/cards.tsx
+++ src/pages/profile/cards.tsx
@@ -1,3 +1,16 @@
-export default function Cards() {
-  return <></>;
+import Profile from 'templates/Profile';
+import type { CardsListProps } from 'components/CardsList';
+import CardsList from 'components/CardsList';
+import mockCards from 'components/PaymentOptions/mock';
+
+export default function ProfileCards({ cards }: CardsListProps) {
+  return (
+    <Profile>
+      <CardsList cards={cards} />
+    </Profile>
+  );
+}
+
+export function getServerSideProps() {
+  return { props: { cards: mockCards } };
 }
