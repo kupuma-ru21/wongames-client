b1a4a34f262ba47f7539e7d7c8f078e02fabf21e
kupuma-ru21 <kupumaru21@gmail.com>
2022-01-01 01:46:27 +0900

    feat: add /profile/orders

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,3 +1,16 @@
-export default function Orders() {
-  return <></>;
+import Profile from 'templates/Profile';
+import type { OrdersListProps } from 'components/OrdersList';
+import OrdersList from 'components/OrdersList';
+import ordersMock from 'components/OrdersList/mock';
+
+export default function Orders({ items }: OrdersListProps) {
+  return (
+    <Profile>
+      <OrdersList items={items} />
+    </Profile>
+  );
+}
+
+export function getServerSideProps() {
+  return { props: { items: ordersMock } };
 }
