bfc509b7241abffb0ccc59229e81ec68a8ef9fa3
kupuma-ru21 <kupumaru21@gmail.com>
2022-01-01 01:40:47 +0900

    feat: add OrdersList

---
+++ src/components/OrdersList/stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import itemsMock from './mock';
+import type { OrdersListProps } from '.';
+import OrdersList from '.';
+
+export default {
+  title: 'Profile/OrdersList',
+  component: OrdersList,
+  args: { items: itemsMock },
+} as Meta;
+
+export const Default: Story<OrdersListProps> = (args) => {
+  return (
+    <div style={{ maxWidth: 850, margin: 'auto' }}>
+      <OrdersList {...args} />
+    </div>
+  );
+};
