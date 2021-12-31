a1bde482e4d25047bd372e322a40f1ce9613f2c3
kupuma-ru21 <kupumaru21@gmail.com>
2021-12-31 12:20:46 +0900

    feat: add ProfileMenu styles and tests

--- src/components/ProfileMenu/index.test.tsx
+++ src/components/ProfileMenu/index.test.tsx
@@ -1,8 +1,21 @@
+import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 import ProfileMenu from '.';
 
 describe('<ProfileMenu />', () => {
-  it.skip('should render the menu', () => {
-    renderWithTheme(<ProfileMenu />);
+  it('should render the menu', () => {
+    const { container } = renderWithTheme(<ProfileMenu />);
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', { name: /my orders/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
