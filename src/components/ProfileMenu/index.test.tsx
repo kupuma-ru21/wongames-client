9ca8af5b77ef043f66b9078aca7fb47eca59cb70
kupuma-ru21 <kupumaru21@gmail.com>
2021-12-31 12:18:25 +0900

    feat: add ProfileMenu structure

---
+++ src/components/ProfileMenu/index.test.tsx
@@ -0,0 +1,8 @@
+import { renderWithTheme } from 'utils/tests/helpers';
+import ProfileMenu from '.';
+
+describe('<ProfileMenu />', () => {
+  it.skip('should render the menu', () => {
+    renderWithTheme(<ProfileMenu />);
+  });
+});
