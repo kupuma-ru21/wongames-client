f0cdbb2976f05c198fc8b90486925bdd96da077c
kupuma-ru21 <kupumaru21@gmail.com>
2022-01-04 13:40:01 +0900

    feat: create ApolloClient directly to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import Home from 'templates/Home';
 import type { HomeTemplateProps } from 'templates/Home';
 import bannersMock from 'components/BannerSlider/mock';
@@ -5,6 +6,21 @@ import gamesMock from 'components/GameCardSlider/mock';
 import highlightMock from 'components/Highlight/mock';
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `,
+  });
+
   return <Home {...props} />;
 }
 
