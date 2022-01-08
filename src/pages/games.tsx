c8bb2ac84652deaaca71fcac5aeb9105a7b76b09
kupuma-ru21 <kupumaru21@gmail.com>
2022-01-08 16:04:05 +0900

    feat: start populating Explore page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,12 +1,56 @@
+import { gql } from '@apollo/client';
 import type { GamesTemplateProps } from 'templates/Games';
 import GamesTemplate from 'templates/Games';
 import filterItemsMock from 'components/ExploreSidebar/mock';
-import gamesMock from 'components/GameCardSlider/mock';
+import { initializeApollo } from 'utils/apollo';
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />;
 }
 
-export async function getServerSideProps() {
-  return { props: { games: gamesMock, filterItems: filterItemsMock } };
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      revalidate: 60,
+      games: data.games.map(
+        (game: {
+          name: string;
+          developers: { name: string }[];
+          cover: { url: string };
+          price: number | bigint;
+        }) => {
+          return {
+            title: game.name,
+            developer: game.developers[0].name,
+            img: `http://localhost:1337${game.cover.url}`,
+            price: new Intl.NumberFormat('en', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(game.price),
+          };
+        }
+      ),
+      filterItems: filterItemsMock,
+    },
+  };
 }
